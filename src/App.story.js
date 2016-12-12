07239e1a2e78bdc5a5feb43e3576b60ebb5cbeaa
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-12-11 19:12:07 -0800

    Add storybook and update config to support *.story.js files.

---
+++ src/App.story.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+
+import App from './App';
+
+storiesOf('App', module)
+  .add('', () => (
+    <App />
+  ));
